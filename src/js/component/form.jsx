e0137e4e08b287d3b9c53ca244bf01d5e030a8c1
Pablo García de Gregorio <129775284+pablogarciadegregorio@users.noreply.github.com>
2023-06-30 09:09:34 +0000

    user y get

--- src/js/component/form.jsx
+++ src/js/component/form.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 
 
@@ -9,6 +9,7 @@ const Form = () => {
 // DECLARACIONDE ESTADOS    
     const[tarea,setTarea] = useState("");
     const[listaTareas, setListaTareas] = useState([]);
+    
 
 //  FORM SUBMIT 
     const handleSubmit = (e) => {
@@ -44,9 +45,66 @@ function borrarTarea(tarea){
     let tareaActual = tarea;
     let tareaBorrada = listaTareas.filter((tarea) => tarea != tareaActual);
     setListaTareas(tareaBorrada);
-    // console.log(tarea);
-    // console.log(tareaBorrada);
+
 }
+
+
+// CREAR USUARIO DE LA API
+
+function createdData() {
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/pgdg',{
+        method:'POST',
+        headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify([]), // body data type must match "Content-Type" header
+    })//busca informacion a la url dada con el metodo especificado
+    .then((response)=>response.json())// => convierto la respuesta buscada en un json => {"info":{},"results":[]} "hola"
+    .then((data)=>console.log(data))// => guardo el json en un espacio de memoria
+    .catch((error)=>console.log(error))// => te aviso si algo sale mal
+}
+
+
+// TRAER LISTA DE TAREAS DE LA API
+
+function getList() {
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/pgdg',{
+        method:'GET'
+    })//busca informacion a la url dada con el metodo especificado
+    .then((response)=>response.json())// => convierto la respuesta buscada en un json => {"info":{},"results":[]} "hola"
+    .then((data)=> {setListaTareas(data.label)
+                    console.log(data.label)})// => guardo el json en un espacio de memoria
+    .catch((error)=>console.log(error))// => te aviso si algo sale mal
+}
+
+// USEEFFECT HACE LAS VECES DE UN ONLOAD
+
+useEffect(()=>{
+    getList()
+    console.log(getList())
+},[])
+
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<ACTUALIZACION, HAY QUE DESAROLLARLO>>>>>>>>>>>>>>>>>>>
+
+function saveList() {
+    fetch('https://assets.breatheco.de/apis/fake/todos/user/pgdg',{
+        method:'PUT',
+        headers: {
+            "Content-Type": "application/json",
+          },
+          body:[],
+    })
+    //busca informacion a la url dada con el metodo especificado
+    .then((response)=>response.json())// => convierto la respuesta buscada en un json => {"info":{},"results":[]} "hola"
+    .then((data)=>console.log(data))// => guardo el json en un espacio de memoria
+    .catch((error)=>console.log(error))// => te aviso si algo sale mal
+}
+
+
+
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>
 
 
 
@@ -55,7 +113,7 @@ function borrarTarea(tarea){
     <>
 
         <h1 className="text-center mt-3">Lista de tareas</h1>
-        <div className="libreta">
+        <div className="libreta justify-content-center">
             <div className="contenedor container m-auto">
                 
                 <form className="formulario" id="Form" onSubmit={handleSubmit} >
@@ -63,14 +121,17 @@ function borrarTarea(tarea){
                 </form>
                 <hr/>
                 <ul className="p-4 pt-0 mb-0">
-                    
-                    {listaHTML}
+                
+                     {listaHTML} 
                 </ul>
                 <p className="tareasPendientes ps-4 pe-4">Quedan {tareasPendientes} tareas pendientes</p>
                 
             </div>
             <div className="hojaAtras1 m-auto"></div>
             <div className="hojaAtras2 m-auto"></div>
+            <button className="btn btn-success d-flex m-auto mt-3" onClick={createdData}>Crear Usuario</button>
+            
+            
         </div>
     </>
 	);
